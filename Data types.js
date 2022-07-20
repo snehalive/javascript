acaf6b3d0d2e4df0cff6c2b7292648c2564c27d0
Sneha <sneha.6@outlook.com>
2022-07-20 16:43:36 +0530

    javascript operators and data types

---
+++ Data types.js
@@ -0,0 +1,25 @@
+// undefined
+var und= undefined;
+var und;
+console.log(und);
+
+// null
+var n = null;
+console.log(n);
+
+// At a very high level, There are two types of data type in javascript
+// 1. Primitive data types: undefined, null, number, string, boolean, symbol
+// 2. Reference data types: 
+
+var arr = [1,2,3,4,5]
+
+// counting start from [0]
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+
+// you can also add string in between arr number
+var arr = [1,me,2,you,3,we]
+
---
+++ operators.js
@@ -0,0 +1,48 @@
+// Operators in javascript
+// Arithmetic Operators
+var a = 2240;
+var b = 56;
+console .log ("The value of a + b is", a+b);
+console .log ("The value of a + b is", a-b);
+console .log ("The value of a + b is", a*b);
+console .log ("The value of a + b is", a/b);
+
+// Assignment operators
+var c=b;
+c+=2;
+c*=2;  c=c-2;
+c-=2;
+c/=2;
+console.log(c);
+
+// comparision operators
+var x= 34;
+var y= 56;
+console.log(x==y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x>y);
+console.log(x<y);
+
+// Logical and operators
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+    
+    // logical OR operator
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false
+
+// Logical not operator
+
+// console.log(false);
+// console.log(!true);
+// console.log(!false);
+
+
+
+
+
