bfabdd87a6e56153a9ee8a42ed29f33b38d1f93c
Sneha <sneha.6@outlook.com>
2022-07-20 20:05:40 +0530

    condition in javascripts

---
+++ function.js
@@ -0,0 +1,12 @@
+// conditionals in javascript
+var age = 34;
+if (age> 8){
+    console.log('you are not a kid')
+}
+else if  (age>18){
+    console.log('you are a teenager')
+}
+else {
+    console.log('you are a kid')
+}
+console.log("End of ladder");
