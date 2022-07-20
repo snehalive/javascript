8747925e4678e944d0b8c4497d48ec0815b60402
Sneha <sneha.6@outlook.com>
2022-07-20 16:27:02 +0530

    add some js file

---
+++ variable.js
@@ -0,0 +1,26 @@
+//  javascript variables
+//   what are variables? - containers to store data values 
+var number1 = 34;
+var number2 = 56;
+console.log(number1+number2)
+// data types in java script
+// number
+var num1= 455;
+var num2= 56.76;
+
+// string
+var str1= "this is a string";
+var str2= 'this is also a string';
+
+// objects
+var marks= {
+    sneha: 56,
+    sweta: 87,
+    atul: 76,
+}
+console.log(marks);
+
+// boolean
+var a = true;
+var b = false;
+console.log(a);
